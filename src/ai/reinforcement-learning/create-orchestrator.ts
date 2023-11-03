f88ba553f62af57724d0ad3e5946d8717f1b58ad
norgeous <daniel@harte.me>
2023-11-03 19:44:46 +0000

    Update create-orchestrator.ts

--- src/ai/reinforcement-learning/create-orchestrator.ts
+++ src/ai/reinforcement-learning/create-orchestrator.ts
@@ -2,13 +2,31 @@ import Phaser from 'phaser';
 import createMemory from './create-memory';
 import createNN from './create-nn';
 
-const createOrchestrator = (scene: Phaser.Scene) => {
+const createOrchestrator = (scene: Phaser.Scene, calculateReward: Function) => {
   const { samples, addSample } = createMemory();
   const model = createNN();
 
   scene.sys.game.scene.start('scene-game');
 
+  calculateReward(scene);
+
   return { samples, addSample, model };
 };
 
 export default createOrchestrator;
+
+const orch = createOrchestrator(this.scene, () => {
+  if (position >= 0.5) {
+    return 100;
+  }
+  if (position >= 0.25) {
+    return 20;
+  }
+  if (position >= 0.1) {
+    return 10;
+  }
+  if (position >= 0) {
+    return 5;
+  }
+  return 0;
+});
