7ddf8084f1c25255d583184ebf688ba48be9b9bc
norgeous <daniel@harte.me>
2023-11-04 09:19:51 +0000

    Update game-scene.ts

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,6 +1,12 @@
 import { Scene, GameObjects } from 'phaser';
 import Ben1 from '../characters/ben1/Ben';
 
+const toggleDebug = (scene: Scene) => {
+  // eslint-disable-next-line no-param-reassign
+  scene.matter.world.drawDebug = !scene.matter.world.drawDebug;
+  scene.matter.world.debugGraphic.clear();
+};
+
 class GameScene extends Scene {
   private ben: Ben1 | undefined;
 
@@ -15,6 +21,10 @@ class GameScene extends Scene {
   }
 
   create() {
+    // toggle debug GFX
+    toggleDebug(this);
+    this.input.keyboard?.on('keydown-CTRL', () => toggleDebug(this));
+
     this.matter.world.setBounds();
     this.matter.add.mouseSpring();
 
