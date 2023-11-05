f82f8114cf42e9fcacb3da83ae172461f6788878
norgeous <daniel@harte.me>
2023-11-05 22:20:05 +0000

    Create Brain.ts

---
+++ src/ai/reinforcement-learning/Brain.ts
@@ -0,0 +1,41 @@
+import * as tf from '@tensorflow/tfjs';
+import createNN from '@/ai/reinforcement-learning/create-nn';
+
+class Brain {
+  public network: tf.Sequential;
+
+  private numActions: number;
+
+  constructor(layerUnits: number[]) {
+    this.network = createNN(layerUnits);
+    this.numActions = layerUnits[layerUnits.length - 1];
+  }
+
+  async train(
+    xBatch: tf.Tensor | tf.Tensor[],
+    yBatch: tf.Tensor | tf.Tensor[],
+  ) {
+    await this.network.fit(xBatch, yBatch);
+  }
+
+  predict(states: tf.Tensor | tf.Tensor[]) {
+    return tf.tidy(() => this.network.predict(states));
+  }
+
+  choose(state: tf.Tensor, eps: number) {
+    // if decayed behave randomly
+    if (Math.random() < eps) {
+      return Math.floor(Math.random() * this.numActions) - 1;
+    }
+
+    // else use NN to predict next action
+    return tf.tidy(() => {
+      const logits = this.predict(state);
+      const sigmoid = tf.sigmoid(logits);
+      const probs = tf.div(sigmoid, tf.sum(sigmoid));
+      return tf.multinomial(probs, 1).dataSync()[0] - 1;
+    });
+  }
+}
+
+export default Brain;
