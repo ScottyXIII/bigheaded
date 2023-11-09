b0d8c1b2279ab07957bdf19fda00d9c05466f65b
norgeous <daniel@harte.me>
2023-11-09 10:30:07 +0000

    Update create-memory.ts

--- src/ai/reinforcement-learning/create-memory.ts
+++ src/ai/reinforcement-learning/create-memory.ts
@@ -1,26 +1,42 @@
 import * as tf from '@tensorflow/tfjs';
 import { sampleSize } from 'lodash';
 
+type RecordType = {
+  state: tf.Tensor;
+  action: number;
+};
+
 type SampleType = {
   state: tf.Tensor;
   action: number;
-  reward: number;
   nextState: tf.Tensor;
+  reward: number;
 };
 
 const createMemory = (maxLength = 500) => {
-  const samples: SampleType[] = [];
+  const samples: RecordType[] = [];
 
-  const addSample = (newSample: SampleType) => {
+  const addSample = (newSample: RecordType) => {
     samples.push(newSample);
     if (samples.length > maxLength) {
-      const { state, nextState } = samples.shift() || {};
+      const { state } = samples.shift() || {};
       state?.dispose();
-      nextState?.dispose();
     }
   };
 
-  const getSamples = (nSamples: number) => sampleSize(samples, nSamples);
+  const getSamples = (nSamples: number, calculateReward: Function) => {
+    // convert Records into Samples
+    const trainingData = samples.reduce((acc, sample) => {
+      const { state, action } = sample;
+      const nextState = state; // TODO
+      const reward = calculateReward(nextState);
+      const newSample = { state, action, reward, nextState };
+      return [...acc, newSample];
+    }, [] as SampleType[]);
+
+    // return randomised samples
+    return sampleSize(trainingData, nSamples);
+  };
 
   return {
     addSample,
