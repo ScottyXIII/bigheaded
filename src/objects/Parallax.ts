6af42f4acd4e60eee103aa847a94062f82118220
norgeous <daniel@harte.me>
2023-11-09 19:44:25 +0000

    save

---
+++ src/objects/Parallax.ts
@@ -0,0 +1,25 @@
+const parallax = (assetName, path, imgCount) => {
+
+  const preLoad = () => {
+    Array.from({length: imgCount}).forEach(index => {
+      this.Phaser.load.image(`background${index}`, `${path}/${index}.png`);
+    });
+  };
+
+
+      const width = this.Phaser.scale.width
+      const height = this.Phaser.scale.height
+      for (let x=1; x <= this.parallax.backgroundCount; x++) {
+        let scrollFactorX = 0.01 + (x/20);
+        let scrollFactorY = 0.01 + (x/50);
+        this.Phaser.add.image(width, height, `background${x}`)
+          .setOrigin(0,0)
+          .setScrollFactor(scrollFactorX, scrollFactorY)
+          .setPosition(0,0)
+          .setSize(width, height)
+      }
+
+  return {preLoad};
+};
+
+export default parallax;
