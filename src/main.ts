9ec4f13ed1c5c5907c70c250f93497dab4057b23
norgeous <daniel@harte.me>
2023-11-09 00:21:35 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Game } from 'phaser';
 
 import config from '@/config';
 import resize from '@/helpers/resize';
+import createOrchestrator from '@/ai/reinforcement-learning/create-orchestrator';
 
 window.onload = () => {
   const game = new Game(config);
@@ -12,4 +13,19 @@ window.onload = () => {
     resize(newConfig);
     game.events.emit('resize');
   };
+
+  (async () => {
+    const { run, replay } = await createOrchestrator(
+      game.scene.scenes[0],
+      () => {
+        // reward func
+      },
+    );
+
+    // @ts-ignore
+    window.run = run;
+
+    // @ts-ignore
+    window.replay = replay;
+  })();
 };
