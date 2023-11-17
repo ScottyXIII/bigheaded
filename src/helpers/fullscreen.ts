16243a26780c898df138e1145ab5482c80376728
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-17 17:43:46 +0000

    Update fullscreen.ts

--- src/helpers/fullscreen.ts
+++ src/helpers/fullscreen.ts
@@ -9,7 +9,7 @@ const attachFullscreen = () => {
         screenfull.request(el, { navigationUI: 'hide' });
       }
 
-      // @ts-ignore
+      // @ts-ignore until https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1615 is closed
       window.screen.orientation.lock('landscape');
     });
   }
