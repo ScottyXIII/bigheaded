6d32cb64d9bb4150fc04d686aae9e05a599f1860
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-17 23:32:00 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const config: Phaser.Types.Core.GameConfig = {
     matter: {
       enableSleeping: true,
       gravity: {
-        y: 4,
+        y: 2,
       },
       ...(isDev && { debug }),
     },
