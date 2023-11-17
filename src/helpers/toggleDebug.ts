8c215f326df0287ff167f5f5aca953afe70d7d1b
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-17 22:47:23 +0000

    "no-param-reassign": ["error", { "props": false }]

--- src/helpers/toggleDebug.ts
+++ src/helpers/toggleDebug.ts
@@ -1,7 +1,6 @@
 import { Scene } from 'phaser';
 
 const toggleDebug = (scene: Scene) => {
-  // eslint-disable-next-line no-param-reassign
   scene.matter.world.drawDebug = !scene.matter.world.drawDebug;
   scene.matter.world.debugGraphic.clear();
 };
