99085807e345f242d0f2efad5c9c0230576a9b92
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-20 15:21:23 +0000

    Update fullscreen.ts

--- src/helpers/fullscreen.ts
+++ src/helpers/fullscreen.ts
@@ -10,6 +10,7 @@ const attachFullscreen = () => {
       }
 
       // because firefox doesnt support .lock ?
+      // https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.screenorientation.html#lock
       // @ts-expect-error until https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1615 is closed
       window.screen.orientation.lock('landscape');
     });
