a6d67c35f7e5989f9f2ffd9d3e13daac08928bdc
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-23 16:34:36 +0000

    Update detectWebGL.ts

--- src/helpers/detectWebGL.ts
+++ src/helpers/detectWebGL.ts
@@ -1,3 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/By_example/Detect_WebGL
+
 const detectWebGLContext = () => {
   const canvas = document.createElement('canvas');
   const gl =
