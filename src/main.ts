0aea12d25715d65059f84efdca68b05cb6ba5304
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-23 16:12:38 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ window.onload = () => {
     return;
   }
 
+  // @ts-expect-error fight me!
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const game = new Phaser.Game(config);
 
