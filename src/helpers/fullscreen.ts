85a73f64cfca2ac7d6ad8c1372612986b391d58b
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-25 17:02:13 +0000

    Update fullscreen.ts

--- src/helpers/fullscreen.ts
+++ src/helpers/fullscreen.ts
@@ -7,21 +7,11 @@ const fullscreenAndLandscape = () => {
 
   // next we change screen orientation to landscape!
 
+  // https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.screenorientation.html#lock
   // this definitly works on chrome
   // because firefox doesnt support .lock ?
-  // https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.screenorientation.html#lock
   // @ts-expect-error until https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1615 is closed
   window.screen.orientation.lock('landscape');
-
-  const lockOrientation =
-    // @ts-expect-error these are deprecated, but might still work
-    window.screen.lockOrientation ||
-    // @ts-expect-error these are deprecated, but might still work
-    window.screen.mozLockOrientation ||
-    // @ts-expect-error these are deprecated, but might still work
-    window.screen.msLockOrientation;
-
-  lockOrientation('landscape');
 };
 
 export default fullscreenAndLandscape;
