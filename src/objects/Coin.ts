a66c20616dd74dd610b2dbf0adb6a1da01d3e2bc
Scott <scotty6663@hotmail.com>
2023-11-25 18:11:44 +0000

    adding coin

---
+++ src/objects/Coin.ts
@@ -0,0 +1,54 @@
+import Entity, { EntityConfigType } from '@/objects/entities/Entity';
+import GameScene from '@/scenes/game-scene';
+import CollisionCategories from '@/enums/CollisionCategories';
+
+const KEY = 'coin';
+
+const entityConfig: EntityConfigType = {
+  name: KEY,
+  spriteSheetKey: KEY,
+  facing: 1,
+  scale: 1,
+  craftpixOffset: {
+    x: 0,
+    y: 0,
+  },
+  physicsConfig: {
+    width: 15,
+    height: 15,
+    chamfer: { radius: 20 },
+  },
+  collisionCategory: CollisionCategories.coin,
+  animations: [
+    {
+      animationKey: 'spin',
+      fps: 12,
+      start: 0,
+      end: 5,
+      repeat: -1,
+    },
+  ],
+};
+
+class Coin extends Entity {
+  public coin: Phaser.GameObjects.Image | undefined;
+
+  static preload(scene: Phaser.Scene) {
+    scene.load.spritesheet({
+      key: KEY,
+      url: './object/items/coins.png',
+      frameConfig: {
+        frameWidth: 16,
+        frameHeight: 16,
+      },
+    });
+  }
+
+  constructor(scene: GameScene, x: number, y: number) {
+    super(scene, x, y, entityConfig);
+    this.scene = scene;
+    this.playAnimation('spin');
+  }
+}
+
+export default Coin;
