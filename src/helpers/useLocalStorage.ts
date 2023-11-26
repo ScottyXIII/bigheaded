5d1c46fd53ba0fb5171054795dfa1ae780f9735a
Daniel Harte <norgeous@users.noreply.github.com>
2023-11-26 01:37:51 +0000

    simplify

--- src/helpers/useLocalStorage.ts
+++ src/helpers/useLocalStorage.ts
@@ -1,16 +1,20 @@
 import { encode, decode } from '@/helpers/safeBase64';
 import { stringify, parse } from '@/helpers/safeJSON';
 
+const keysToEncrypt = ['coins'];
+
 const useLocalStorage = (
   key: string,
   defaultValue: string | number | boolean,
-  encrypt = false,
 ) => {
+  const encryptOn = keysToEncrypt.includes(key);
   const lsValue = String(window.localStorage.getItem(key));
-  const value = parse(encrypt ? decode(lsValue) : lsValue) || defaultValue;
+  const value = parse(encryptOn ? decode(lsValue) : lsValue) || defaultValue;
 
   const setValue = (newValue: string | number | boolean) => {
-    const encoded = encrypt ? encode(stringify(newValue)) : stringify(newValue);
+    const encoded = encryptOn
+      ? encode(stringify(newValue))
+      : stringify(newValue);
     window.localStorage.setItem(key, encoded);
   };
 
