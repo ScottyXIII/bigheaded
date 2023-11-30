8bcc9a1eb631f5de9564e81dd18a51577350c089
Scott <scotty6663@hotmail.com>
2023-11-30 19:03:43 +0000

    allow 1 jump in air

--- src/objects/entities/Ben3.ts
+++ src/objects/entities/Ben3.ts
@@ -102,6 +102,8 @@ class Ben3 extends Entity {
 
   public headScale = HEAD_SCALE_MIN;
 
+  private isJumping = false;
+
   static preload(scene: Phaser.Scene) {
     scene.load.spritesheet({
       key: KEY,
@@ -164,23 +166,24 @@ class Ben3 extends Entity {
   }
 
   jump() {
-    if (this.sensorData.bottom.size >= 1) {
-      this.scene.audio?.playAudio('jump');
+    if (this.isJumping) return;
+    this.isJumping = true;
 
-      const { body: Body } = this.scene.matter;
+    this.scene.audio?.playAudio('jump');
 
-      const { centerX, centerY } = this.gameObject.getBounds();
-      const position = { x: centerX, y: centerY };
-      Body.applyForce(this.gameObject.body, position, {
-        x: 0,
-        y: -0.05 * this.gameObject.body.mass,
-      });
+    const { body: Body } = this.scene.matter;
 
-      Body.applyForce(this.head.body, this.head.getCenter(), {
-        x: 0,
-        y: -0.05 * this.head.body.mass,
-      });
-    }
+    const { centerX, centerY } = this.gameObject.getBounds();
+    const position = { x: centerX, y: centerY };
+    Body.applyForce(this.gameObject.body, position, {
+      x: 0,
+      y: -0.05 * this.gameObject.body.mass,
+    });
+
+    Body.applyForce(this.head.body, this.head.getCenter(), {
+      x: 0,
+      y: -0.05 * this.head.body.mass,
+    });
   }
 
   turnDirection(angularVelocity: number) {
@@ -219,6 +222,11 @@ class Ben3 extends Entity {
 
   update(time: number, delta: number) {
     super.update(time, delta);
+
+    // rest jump on landing
+    if (this.isJumping && this.sensorData.bottom.size >= 1) {
+      this.isJumping = false;
+    }
 
     if (this.sensorData.bottom.size >= 1) {
       if (!this.scene.goal) return;
